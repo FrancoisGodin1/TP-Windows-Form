97cb0e74301f9134a4a800fcb958a1ca102c36a3
François Godin <francois.h.godin@gmail.com>
2017-04-06 23:23:01 +0200

    Add files via upload

---
+++ Ffille.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tpFentreMereFille
+{
+    public partial class Ffille : Form
+    {
+
+        int numeroFille;
+        Fmere maMere;
+        
+        
+
+        
+
+        public Ffille(int numeroFille, Fmere maMere)
+        {
+            InitializeComponent();
+            this.numeroFille = numeroFille;
+            this.maMere = maMere;
+            this.Text = this.ToString();
+            btnMamere.Click += new EventHandler(btnMamere_Click);
+            btnChanger.Click += new EventHandler(btnChanger_Click);
+            this.Load += new EventHandler(Ffille_Load);
+            
+                                            
+        }
+
+        
+       
+
+        void btnChanger_Click(object sender, EventArgs e)
+        {
+            this.Text = tbNom.Text; 
+            // plus besoin d'appeler MafilleChangeDeNom grace aux deuxieme cas ou la mere sabonne a l'évenement textchanged de sa fille instancié 
+            //maMere.MaFilleChangeDeNom(this, this.Text);
+        }
+
+        void Ffille_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Evenement load sur la fenetre"+this.numeroFille);
+        }
+
+        void btnMamere_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(maMere.GetNomMere());
+        }
+
+        public override string ToString()
+        {
+            return String.Format("n°{0} : {1}", this.numeroFille,this.Text);
+        }
+
+        public int GetNumFille()
+        {
+            return this.numeroFille;
+        }
+
+        //annule l'evenement Close et le change en Hide()
+ 
+        //private void Ffille_FormClosing(object sender, FormClosingEventArgs e)
+        //{
+        //    if (e.CloseReason == CloseReason.UserClosing)
+        //    {
+        //        e.Cancel = true;
+        //        this.Hide();
+        //    }
+        //}
+
+
+        
+
+
+
+
+
+        
+    }
+}
---
+++ Fmere.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tpFentreMereFille
+{
+    public partial class Fmere : Form
+    {
+        private List<Ffille> lesFilles;
+        int numerofille;
+        string nomMere;
+        
+
+        public Fmere()
+        {
+            InitializeComponent();
+            lesFilles = new List<Ffille>();
+            btnNew.Click += new EventHandler(btnNew_Click);
+            btnShow.Click += new EventHandler(btnShow_Click);
+            btnNew.Click +=new EventHandler(btnNew_Click_Msg);
+            btnHide.Click += new EventHandler(btnHide_Click);
+            btnShowDialog.Click += new EventHandler(btnShowDialog_Click);
+            btnClose.Click += new EventHandler(btnClose_Click);
+            
+
+            this.Text = nomMere;
+            this.nomMere = "Maman";
+        }
+
+        void btnClose_Click(object sender, EventArgs e)
+        {
+            lesFilles[lbFille.SelectedIndex].Close();
+        }
+
+        void btnShowDialog_Click(object sender, EventArgs e)
+        {
+            lesFilles[lbFille.SelectedIndex].ShowDialog();
+        }
+
+        void btnHide_Click(object sender, EventArgs e)
+        {
+            lesFilles[lbFille.SelectedIndex].Hide();
+        }
+
+        void btnShow_Click(object sender, EventArgs e)
+        {
+            if ( lbFille.SelectedIndex != -1 )
+                lesFilles[lbFille.SelectedIndex].Show();
+        }
+
+        void btnNew_Click(object sender, EventArgs e)
+        {
+            this.numerofille = this.numerofille + 1;
+            Ffille nouvelleFille = new Ffille(this.numerofille,this);
+            nouvelleFille.TextChanged += new EventHandler(nouvelleFille_TextChanged);
+            nouvelleFille.FormClosing += new FormClosingEventHandler(nouvelleFille_FormClosing);
+            lesFilles.Add(nouvelleFille);
+            lbFille.Items.Add(nouvelleFille.ToString());
+
+        }
+
+        void nouvelleFille_TextChanged(object sender, EventArgs e)
+        {
+            Ffille f = (Ffille)sender;
+            int i = lesFilles.IndexOf(f);
+            lbFille.Items.RemoveAt(i);
+            lbFille.Items.Insert(i, f.Text);            
+        }
+
+        void nouvelleFille_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Ffille f = (Ffille)sender;
+            lbFille.Items.RemoveAt(lesFilles.IndexOf(f));
+            lesFilles.RemoveAt(lesFilles.IndexOf(f));
+            
+        }
+
+        void btnNew_Click_Msg(object sender, EventArgs e)
+        {
+            MessageBox.Show("Une fille a été instancié");
+        }
+
+        public string GetNomMere() 
+        {
+            return this.nomMere;
+        }
+
+        //public void MaFilleChangeDeNom(Ffille laFille, string nouveauNom)
+        //{
+        //    int i = lesFilles.IndexOf(laFille);
+        //    lbFille.Items.RemoveAt(i);           
+        //    lbFille.Items.Insert(i, laFille.Text);
+            
+        //}
+
+
+        
+
+        
+
+        
+    }
+}
